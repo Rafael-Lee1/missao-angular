841c1df78fa4f9f463d25576b0d72be3007d4a13
Rafael-Lee1 <rafael-floripa@hotmail.com.br>
2024-10-09 17:26:48 -0300

    Add .mailmap for contributor display

--- src/app/data/post.ts
+++ src/app/data/post.ts
@@ -8,18 +8,18 @@ import { Blog } from '../component/blog';
 export class Post implements InMemoryDbService {
   createDb() {
     const blogs = [
-      { 
-  id: 0, 
+      {
+  id: 0,
   name: 'Pre Install' ,
   description: 'Requipment for first installation' ,
   cover: 'https://astronot.axcora.com/img/ASTRONOTCSS.jpg' ,
   content: 'For first installation you need to download node or npm so download node first from https://nodejs.org/en/download/ then install on your devices, after node installation success , now you need to install angular globaly on your devices just run npm install -g @angular/cli'
   },
-  { id: 1, 
+  { id: 1,
    name: 'Installation' ,
    description: 'How to install astronot angular starter' ,
    cover: 'https://astronot.axcora.com/img/install.jpg' ,
-   content: 'After you have installing node and angular globaly on your devices, now we ready to install astronot angular, for first download source code on our repo https://github.com/mesinkasir/astronot-angular , extract all file folder and open terminal run npm install for install astronot angular.'
+   content: 'After you have installing node and angular globaly on your devices, now we ready to install astronot angular, for first download source code on our repo https://github.com/Rafael-Lee1 , extract all file folder and open terminal run npm install for install astronot angular.'
    },
   { id: 2,
   name: 'Start' ,
@@ -38,7 +38,7 @@ export class Post implements InMemoryDbService {
     description: 'How to create article blog content' ,
     cover: 'https://astronot.axcora.com/img/form.jpg' ,
     content: 'Now you can easy for insert or create and edit content article blog post, you can visit on data folder then open post.ts file and create new or update content in here , then save it for update your content blog post.'
-    },  
+    },
   { id: 5,
   name: 'Layout' ,
   description: 'Template Layout design astronot angular' ,
@@ -49,7 +49,7 @@ export class Post implements InMemoryDbService {
   name: 'Deploy' ,
   description: 'Run your astronot angular online on hosting' ,
   cover: 'https://astronot.axcora.com/img/webapp.jpg' ,
-  content: 'After you have finished astronot angular project and you need to deploy on static hosting , so for first you need to update your project on production mode, open terminal and run ng build, then you can push dist folder file on static hosting, you can use firebase, vercel, heroku, cloudflare , surge and more.. For fast build production mode, you just need to register and create account on github then fork our repo on github https://github.com/mesinkasir/astronot-angular , and register create account usign gihtub account on static host like vercel, heroku, and create new project , then select your fork repo and deploy it... and congratulations your astronot angular web app is online now.'
+  content: 'After you have finished astronot angular project and you need to deploy on static hosting , so for first you need to update your project on production mode, open terminal and run ng build, then you can push dist folder file on static hosting, you can use firebase, vercel, heroku, cloudflare , surge and more.. For fast build production mode, you just need to register and create account on github then fork our repo on github https://github.com/Rafael-Lee1 , and register create account usign gihtub account on static host like vercel, heroku, and create new project , then select your fork repo and deploy it... and congratulations your astronot angular web app is online now.'
   },
   { id: 7,
   name: 'Coffee' ,
